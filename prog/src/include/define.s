a5a1975545b90626dff0077422a0ac2913d2d721
hahahayatoo <hzzkwepufd@gmail.com>
2020-04-25 20:36:53 +0900

    update: カーネルをロードする

--- prog/src/include/define.s
+++ prog/src/include/define.s
@@ -1,7 +1,13 @@
-        BOOT_LOAD   equ     0x7C00                  ; ブートプログラムのロード位置
+        BOOT_LOAD           equ     0x7C00                  ; ブートプログラムのロード位置
 
-        BOOT_SIZE   equ     (1024 * 8)              ; ブートコードサイズ
-        SECT_SIZE   equ     (512)                   ; セクタサイズ
-        BOOT_SECT   equ     (BOOT_SIZE / SECT_SIZE) ; ブートプログラムのセクタ数
+        BOOT_SIZE           equ     (1024 * 8)              ; ブートコードサイズ
+        SECT_SIZE           equ     (512)                   ; セクタサイズ
+        BOOT_END            equ     (BOOT_LOAD + BOOT_SIZE)
 
-        E820_RECORD_SIZE    equ     20              ; BIOSで取得したメモリ情報を格納する領域のサイズ
+        E820_RECORD_SIZE    equ     20                      ; BIOSで取得したメモリ情報を格納する領域のサイズ
+
+        KERNEL_LOAD         equ     0x0010_1000
+        KERNEL_SIZE         equ     (1024 * 8)              ; カーネルサイズ    
+        
+        BOOT_SECT           equ     (BOOT_SIZE / SECT_SIZE) ; ブートプログラムのセクタ数
+        KERNEL_SECT         equ     (KERNEL_SIZE / SECT_SIZE)
