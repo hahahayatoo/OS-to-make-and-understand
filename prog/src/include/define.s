aef0261b94275a613878607e0cffb49be98c10ca
hahahayatoo <hzzkwepufd@gmail.com>
2020-05-04 15:09:53 +0900

    update: 割り込みゲートディスクリプタを作成する

--- prog/src/include/define.s
+++ prog/src/include/define.s
@@ -6,8 +6,10 @@
 
         E820_RECORD_SIZE    equ     20                      ; BIOSで取得したメモリ情報を格納する領域のサイズ
 
+        VECT_BASE           equ     0x0010_0000             ; 0010_0000:0010_07FF
+
         KERNEL_LOAD         equ     0x0010_1000
-        KERNEL_SIZE         equ     (1024 * 8)              ; カーネルサイズ    
-        
+        KERNEL_SIZE         equ     (1024 * 8)              ; カーネルサイズ
+
         BOOT_SECT           equ     (BOOT_SIZE / SECT_SIZE) ; ブートプログラムのセクタ数
         KERNEL_SECT         equ     (KERNEL_SIZE / SECT_SIZE)
